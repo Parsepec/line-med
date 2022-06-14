a76c582bc5601d7194c781606ef81938c20499cf
parsepec <parsepecq@gmail.com>
2022-06-14 09:18:25 +0100

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from 'astro/config';
+
+import react from "@astrojs/react";
+import vue from "@astrojs/vue";
+import tailwind from "@astrojs/tailwind";
+import partytown from "@astrojs/partytown";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [react(), vue(), tailwind(), partytown()]
+});
