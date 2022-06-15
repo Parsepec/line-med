3ea882e2707274c84b0b958ca67bdff9bb69f461
parsepec <parsepecq@gmail.com>
2022-06-15 11:43:45 +0100

    Added components

--- src/components/Swipe.vue
+++ src/components/Swipe.vue
@@ -5,7 +5,7 @@
     @swiper="onSwiper"
     @slideChange="onSlideChange"
   >
-    <swiper-slide><div><img :src="aboutimg" class="w-full h-screen" alt=""></div> </swiper-slide>
+    <swiper-slide><div class="relative"><img :src="banner2" class="w-full h-screen" alt=""></div> </swiper-slide>
     <swiper-slide>Slide 2</swiper-slide>
     <swiper-slide>Slide 3</swiper-slide>
     ...
@@ -14,10 +14,12 @@
 <script setup lang="ts">
 // Import Swiper Vue.js components
 import aboutimg from '../images/linemed-about.jpg';
+import banner from '../images/linemed-banner.jpg'
+import banner2 from '../images/linemed-banner2.jpeg'
 import { Swiper, SwiperSlide } from "swiper/vue";
 
 // Import Swiper styles
-import "swiper/css";
+import "swiper/css/bundle";
 
 const onSwiper = (swiper) => {
   console.log(swiper);
