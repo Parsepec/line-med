7d886c880d19e16550a40294416b7ee0ce5789a8
parsepec <parsepecq@gmail.com>
2022-06-15 12:24:25 +0100

    updated

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,13 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+
+				fontFamily: {
+				  'header': 'Merriweather, serif',
+				  'open':['Open Sans','sans-serif']
+				}
+		},
 	},
 	plugins: [],
 }
